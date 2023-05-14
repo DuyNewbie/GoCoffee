f978f5aed59670b677f831a3ae3ba4f614222fd1
HungVu12112 <hungmno123@gmail.com>
2023-05-15 00:42:58 +0700

    navigation demo
    navigation demo

--- app/src/main/java/com/example/gocoffee/MainActivity.java
+++ app/src/main/java/com/example/gocoffee/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.gocoffee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.gocoffee.fragment.AccountFragment;
+import com.example.gocoffee.fragment.CartFragment;
+import com.example.gocoffee.fragment.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +27,30 @@ public class MainActivity extends AppCompatActivity {
         String Hoangseven = "test pust code to git 1";
         String HungVh = "test pust code to git 1";
         String hiepDeepTry = "Hiệp đẹp trai vô địch vũ trụ";
+
+        navigationView = findViewById(R.id.bottomNavigationView);
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.navigation_home) {
+                    replaceFrament(new HomeFragment());
+                } else if (itemId == R.id.navigation_cart) {
+                    replaceFrament(new CartFragment());
+                } else if (itemId == R.id.navigation_account) {
+                    replaceFrament(new AccountFragment());
+                }
+                return true;
+            }
+        });
+
+
+    }
+    private  void  replaceFrament(Fragment fragment){
+        FragmentManager fragmentManager  = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout,fragment);
+        transaction.commit();
+
     }
 }
