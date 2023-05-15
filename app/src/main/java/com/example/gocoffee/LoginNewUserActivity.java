dd710e819d793f3d69faed5b247e8cc6ee265682
Hoangseven7 <tatranminhhoangseven@gmail.com>
2023-05-16 00:40:46 +0700

    Hoang Login

---
+++ app/src/main/java/com/example/gocoffee/LoginNewUserActivity.java
@@ -0,0 +1,14 @@
+package com.example.gocoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginNewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_new_user);
+    }
+}
